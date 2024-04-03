c7898ebfbc7df00407712957c0dca2a81c1707fb
1N73RC3P70R <kras.konstantinov@yandex.ru>
2024-04-03 06:54:23 +0300

    Initial Commit

--- src/test/kotlin/NotesTest.kt
+++ src/test/kotlin/NotesTest.kt
@@ -13,33 +13,33 @@ class NotesServiceTest {
 
     @Test
     fun testAddNote() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        assertEquals(1, notesService.get("Author").size)
+        assertEquals(1, notesService.get("Автор").size)
     }
 
     @Test
     fun testCreateComment() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        val comment = Comment(1, 1, "Comment", "User")
+        val comment = Comment(1, 1, "Комментарий", "Пользователь")
         notesService.createComment(1, comment)
         assertEquals(1, notesService.getComments(1).size)
     }
 
     @Test
     fun testDeleteNote() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
         notesService.delete(1)
-        assertTrue(notesService.get("Author").isEmpty())
+        assertTrue(notesService.get("Автор").isEmpty())
     }
 
     @Test
     fun testDeleteComment() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        val comment = Comment(1, 1, "Comment", "User")
+        val comment = Comment(1, 1, "Комментарий", "Пользователь")
         notesService.createComment(1, comment)
         notesService.deleteComment(1)
         assertTrue(notesService.getComments(1).isEmpty())
@@ -47,55 +47,55 @@ class NotesServiceTest {
 
     @Test
     fun testEditNote() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        notesService.edit(1, "New Title", "New Content")
+        notesService.edit(1, "Новый Заголовок", "Новое Содержание")
         val updatedNote = notesService.getById(1)
-        assertEquals("New Title", updatedNote?.title)
-        assertEquals("New Content", updatedNote?.content)
+        assertEquals("Новый Заголовок", updatedNote?.title)
+        assertEquals("Новое Содержание", updatedNote?.content)
     }
 
     @Test
     fun testEditComment() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        val comment = Comment(1, 1, "Comment", "User")
+        val comment = Comment(1, 1, "Комментарий", "Пользователь")
         notesService.createComment(1, comment)
-        notesService.editComment(1, "New Comment")
+        notesService.editComment(1, "Новый Комментарий")
         val updatedComment = notesService.getComments(1).firstOrNull()
-        assertEquals("New Comment", updatedComment?.content)
+        assertEquals("Новый Комментарий", updatedComment?.content)
     }
 
     @Test
     fun testGetNotesByAuthor() {
-        notesService.add(Note(1, "Title1", "Content1", "Author1"))
-        notesService.add(Note(2, "Title2", "Content2", "Author2"))
-        notesService.add(Note(3, "Title3", "Content3", "Author1"))
-        assertEquals(2, notesService.get("Author1").size)
+        notesService.add(Note(1, "Заголовок 1", "Содержание 1", "Автор 1"))
+        notesService.add(Note(2, "Заголовок 2", "Содержание 2", "Автор 2"))
+        notesService.add(Note(3, "Заголовок 3", "Содержание 3", "Автор 1"))
+        assertEquals(2, notesService.get("Автор 1").size)
     }
 
     @Test
     fun testGetNoteById() {
-        notesService.add(Note(1, "Title1", "Content1", "Author"))
+        notesService.add(Note(1, "Заголовок 1", "Содержание 1", "Автор"))
         val note = notesService.getById(1)
-        assertEquals("Title1", note?.title)
-        assertEquals("Content1", note?.content)
+        assertEquals("Заголовок 1", note?.title)
+        assertEquals("Содержание 1", note?.content)
     }
 
     @Test
     fun testGetCommentsForNote() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        notesService.createComment(1, Comment(1, 1, "Comment1", "User1"))
-        notesService.createComment(1, Comment(2, 1, "Comment2", "User2"))
+        notesService.createComment(1, Comment(1, 1, "Комментарий 1", "Пользователь 1"))
+        notesService.createComment(1, Comment(2, 1, "Комментарий 2", "Пользователь 2"))
         assertEquals(2, notesService.getComments(1).size)
     }
 
     @Test
     fun testRestoreDeletedComment() {
-        val note = Note(1, "Title", "Content", "Author")
+        val note = Note(1, "Заголовок", "Содержание", "Автор")
         notesService.add(note)
-        val comment = Comment(1, 1, "Comment", "User")
+        val comment = Comment(1, 1, "Комментарий", "Пользователь")
         notesService.createComment(1, comment)
         notesService.deleteComment(1)
         notesService.restoreComment(1)
